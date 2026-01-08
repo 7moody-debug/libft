5e6887d0eb535be02c1858c7143c75575878c21b
7moody-debug <mohammadunknown99@gmail.com>
2026-01-08 15:13:37 +0300

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,78 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return (&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*res;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!res)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2 && s2[j])
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	res[i] = '\0';
+	free(s1);
+	return (res);
+}
+
+char	*ft_substr(char *s, int start, int len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	if (!s || len <= 0)
+		return (NULL);
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
