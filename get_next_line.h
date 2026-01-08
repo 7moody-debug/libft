b9b6b556bb34fe1b6a19dece7c1f64888eedd3bc
7moody-debug <mohammadunknown99@gmail.com>
2026-01-08 15:13:23 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, int start, int len);
+
+#endif
